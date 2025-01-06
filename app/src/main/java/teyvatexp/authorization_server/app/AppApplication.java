341de5cbe1d87f7209e7f3ec710ac9aa1e283f9f
sprokopiev <sprokopievjava@gmail.com>
2025-01-06 22:03:57 +0300

    Add env variables
    Also add component scan for other modules

--- app/src/main/java/teyvatexp/authorization_server/app/AppApplication.java
+++ app/src/main/java/teyvatexp/authorization_server/app/AppApplication.java
@@ -2,8 +2,18 @@ package teyvatexp.authorization_server.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"teyvatexp.authorization_server.core",
+		"teyvatexp.authorization_server.app",
+		"teyvatexp.authorization_server.storage"
+})
+@EnableJpaRepositories(basePackages = "teyvatexp.authorization_server.storage.repository")
+@EntityScan(basePackages = "teyvatexp.authorization_server.storage.entity")
 public class AppApplication {
 
 	public static void main(String[] args) {
